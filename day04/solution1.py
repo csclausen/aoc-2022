6e3591a880ed8e1ef3818eaaf41bbc9f2d5b5970
csclausen <csclausen92@gmail.com>
2022-12-07 14:09:57 -0700

    Day 4 solutions

---
+++ day04/solution1.py
@@ -0,0 +1,41 @@
+def get_elf_range(elf_zones):
+    # looks like 2-4
+    elf_range = elf_zones.split("-")
+    return elf_range[0], elf_range[1]
+
+def split_elf_ranges(input_line):
+    # looks like 2-4,6-8
+    elves = input_line.split(",")
+    return elves[0], elves[1]
+
+def ranges_intersect(e1_range, e2_range):
+    return elf_range_inside(e1_range, e2_range) or elf_range_inside(e2_range, e1_range)
+
+def elf_range_inside(e1_range, e2_range):
+    return int(e1_range[0]) <= int(e2_range[0]) and int(e1_range[1]) >= int(e2_range[1])
+
+def elves_intersect(e1_range, e2_range):
+    if int(e1_range[1]) >= int(e2_range[0]) and int(e1_range[1]) <= int(e2_range[1]):
+        return True
+    elif int(e1_range[0]) >= int(e2_range[0]) and int(e1_range[0]) <= int(e2_range[1]):
+        return True
+    return False
+
+
+        
+
+def main():
+    pairs = 0
+    import pdb
+    for line in open("input.txt"):
+        elf1, elf2 = split_elf_ranges(line)
+        e1_range = get_elf_range(elf1)
+        e2_range = get_elf_range(elf2)
+        if elves_intersect(e1_range, e2_range) or elves_intersect(e2_range, e1_range):
+            pairs += 1
+    
+    print(pairs)
+        
+
+
+main()
